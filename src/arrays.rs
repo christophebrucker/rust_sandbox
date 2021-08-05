c67b2bcbf1f3c1770ad156662588dd1f528b551a
Christophe Brucker <christophe.brucker@gmail.com>
2021-08-05 21:40:15 +0200

    change2

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,6 +1,7 @@
 // fixed list where the elements are the same datatypes
 // 49:04
 // practice to do every day ?
+// do it on friday
 pub fn run() {
   let numbers: [i32; 5] = [1, 2, 3, 4, 5];
   println!("{:?}", numbers);
